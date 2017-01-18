cb4e5ef15a855d68c141f70d46530b8c0fcd4429
Gary Taylor <gtaylor@electricwerks.com>
2017-01-18 16:15:12 -0700

    travis fixes

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', '127.0.0.1'),
             'port'      => env('DB_PORT', '3306'),
-            'database'  => env('DB_DATABASE', 'bands_test'),
+            'database'  => env('DB_DATABASE', 'test'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
