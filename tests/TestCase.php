c9f2c82bcd9116987778f86085cc91d651eadef4
Gary Taylor <gtaylor@electricwerks.com>
2017-01-25 09:41:50 -0700

    update tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        Artisan::call('db:seed', ['--database' => 'testing']);
+    }
+
+    public function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
 }
